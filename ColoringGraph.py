0287aee875b9cd9d1b518b3a14d7c645149911ec
PHentosh <71725222+PHentosh@users.noreply.github.com>
2021-06-09 11:24:15 +0300

    Update ColoringGraph.py

--- ColoringGraph.py
+++ ColoringGraph.py
@@ -11,7 +11,7 @@ class Graph():
 	# A utility function to check
 	# if the current color assignment
 	# is safe for vertex v
-	def isSafe(self, v, colour, c):
+	def is_safe(self, v, colour, c):
 		for i in range(self.V):
 			if self.graph[v][i] == 1 and colour[i] == c:
 				return False
@@ -19,20 +19,20 @@ class Graph():
 	
 	# A recursive utility function to solve m
 	# coloring problem
-	def graphColourUtil(self, m, colour, v):
+	def graph_colour_util(self, m, colour, v):
 		if v == self.V:
 			return True
 
 		for c in range(1, m + 1):
-			if self.isSafe(v, colour, c) == True:
+			if self.is_safe(v, colour, c) == True:
 				colour[v] = c
-				if self.graphColourUtil(m, colour, v + 1) == True:
+				if self.graph_colour_util(m, colour, v + 1) == True:
 					return True
 				colour[v] = 0
 
-	def graphColouring(self, m):
+	def graph_colouring(self, m):
 		colour = [0] * self.V
-		if self.graphColourUtil(m, colour, 0) == None:
+		if self.graph_colour_util(m, colour, 0) == None:
 			return False
 
 		# Print the solution
